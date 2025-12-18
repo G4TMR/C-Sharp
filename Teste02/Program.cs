1abcbb44fc6831cd1ef9f913f1868f5e846ff997
Aprendiz Ti <Aprendiz_Ti@amcel.stn>
2025-12-18 14:27:52 -0300

    1

---
+++ Teste02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Principal
+{
+    static void Main()
+    {
+        double nota1, nota2, nota3, media;
+        Console.WriteLine("Digite a primeira nota:");
+        nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite a segunda nota:");
+        nota2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite a terceira nota:");
+        nota3 = Convert.ToDouble(Console.ReadLine());
+        media = (nota1 + nota2 + nota3) / 3;
+        Console.WriteLine ("A média é: " + media.ToString("F2"));
+        if (media >= 6.0)
+        { Console.WriteLine("Aprovado");}
+        else
+        { Console.WriteLine("Reprovado");}
+
+    }
+}
